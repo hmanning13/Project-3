d8fd08ba611f49eaa59e049b76210ce7bdf807bc
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-16 12:46:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ Interactive Forms*/
 const name = document.getElementById("name");
 const otherTitle = document.getElementById("other-title");
 const title = document.getElementById("title");
-
-
+const shirtColorOptions = document.getElementById("color");
+const shirtDesignSelect = document.getElementById("design");
 
 
 //Function used to focus on name field when page is first loaded
@@ -25,6 +25,10 @@ focusName();
 //document.getElementById("idOfTextField").style.display = "block";
 
 
+
+
+//Function to hide "other" textfield when page is first loaded
+
 otherTitle.style.display = "none";
 
 title.addEventListener("change", (event) => {
@@ -34,4 +38,37 @@ title.addEventListener("change", (event) => {
         } else {
         otherTitle.style.display = "none";
         }
+});
+
+
+
+
+
+
+
+
+shirtDesignSelect.addEventListener("change", (event) =>{
+    
+    if(event.target.value === "js puns"){
+        shirtColorOptions[0].style.display = "block";
+        shirtColorOptions[1].style.display = "block";
+        shirtColorOptions[2].style.display = "block";
+        shirtColorOptions[3].style.display = "none";
+        shirtColorOptions[4].style.display = "none";
+        shirtColorOptions[5].style.display = "none";
+        shirtColorOptions[0].selected = true;
+    }
+
+
+
+    if(event.target.value === "heart js"){
+        shirtColorOptions[0].style.display = "none";
+        shirtColorOptions[1].style.display = "none";
+        shirtColorOptions[2].style.display = "none";
+        shirtColorOptions[3].style.display = "block";
+        shirtColorOptions[4].style.display = "block";
+        shirtColorOptions[5].style.display = "block";
+        shirtColorOptions[3].selected = true;
+    }
+
 });
