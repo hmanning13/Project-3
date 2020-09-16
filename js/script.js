4620d0bbf406725eb73039ebe7b5cb827c61a4e1
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-16 12:10:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
 /*Treehouse Techdegree Project 3
 Interactive Forms*/
 
-//Function used to focus on name field when page is first loaded
-const name = document.getElementById("name");
 
+
+//Variables
+const name = document.getElementById("name");
+const otherTitle = document.getElementById("other-title");
+const title = document.getElementById("title");
+
+
+
+
+//Function used to focus on name field when page is first loaded
 const focusName = () => {
     name.focus();
 }
 focusName();
 
+
+
+//document.getElementById("idOfTextField").style.display = "none";
+
+//when active...
+//document.getElementById("idOfTextField").style.display = "block";
+
+
+otherTitle.style.display = "none";
+
+title.addEventListener("change", (event) => {
+
+    if(event.target.value === "other"){
+        otherTitle.style.display = "block";
+        } else {
+        otherTitle.style.display = "none";
+        }
+});
