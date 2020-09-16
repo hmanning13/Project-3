d3bbcbcfabf2b35001f6799afe635a99e6693430
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-16 11:10:41 -0400

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*Treehouse Techdegree Project 3
+Interactive Forms*/
+
+//Function used to focus on name field when page is first loaded
+const name = document.getElementById("name");
+
+const focusName = () => {
+    name.focus();
+}
+focusName();
+
