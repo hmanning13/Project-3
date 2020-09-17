0996e9f35df79ef945a35c513248668c8d08f357
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-17 11:16:49 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -200,20 +200,32 @@ name.addEventListener("change", () => {
 
 
 //Email Validation
-const email = document.getElementById("mail");
+const userEmail = document.getElementById("mail");
 
-const emailValidation = () => {
-    const custEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    //email regex from: https://emailregex.com
-    if (custEmail.test(email.value)) {
-        email.style.borderColor = "green";
+
+
+
+
+const userEmailValidation = () => {
+    const testEmail =  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    if (testEmail.test(userEmail.value)) {
+        userEmail.style.borderColor = "green";
         return true;
     } else {
-        email.style.color.borderColor = "red";
+        userEmail.style.borderColor = "red";
         return false;
     }
 }
 
-email.addEventListener("input", () => {
-    emailValidation();
+
+userEmail.addEventListener("input", () => {
+    userEmailValidation();
 });
+
+
+
+
+
+
+
